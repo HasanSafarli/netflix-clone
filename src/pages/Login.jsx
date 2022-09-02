fa5b798fc2119bb4b12b361600ce92c1dcd9b7d3
HasanSafarli <hsnsfrli@gmail.com>
2022-09-02 19:45:17 -0400

    login bug fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate} from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('')
+  const { user, logIn } = UserAuth();
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('')
+    try {
+      await logIn(email, password);
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+      setError(error.message)
+    }
+  };
+
+
   return (
     <div className='w-full h-screen'>
             <img
@@ -14,19 +34,20 @@ const Login = () => {
               <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
                 <div className='max-w-[320px] mx-auto py-16'>
                   <h1 className='text-3xl font-bold'>Sign In</h1>
+                  {error ? <p className='p-2 bg-red-600 my-3'>{error}</p> : null}
                   <form
-                  
+                    onSubmit={handleSubmit}
                     className='w-full flex flex-col py-4'
                   >
                     <input
-                      
+                      onChange={(e) => setEmail(e.target.value)}
                       className='p-3 my-2 bg-gray-700 rounded'
                       type='email'
                       placeholder='Email'
                       autoComplete='email'
                     />
                     <input
-                     
+                      onChange={(e) => setPassword(e.target.value)}
                       className='p-3 my-2 bg-gray-700 rounded'
                       type='password'
                       placeholder='Password'
