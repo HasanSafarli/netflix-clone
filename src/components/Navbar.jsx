aaef41da30ab3204156cb3ebf5f452745b1cdd57
HasanSafarli <hsnsfrli@gmail.com>
2022-09-02 19:16:03 -0400

    fix bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,46 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 
-const Navbar = () => (
+const Navbar = () => {
+    const {user, logOut} = UserAuth()
+    const navigate = useNavigate()
+    // console.log(user)
+
+    const handleLogout = async () => {
+        try {
+            await logOut()
+            navigate('/')
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
         <Link to='/'>
             <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
         </Link>
+        {user?.email ? <div>
+            <Link to='account'>
+                <button className='text-white pr-4'>Account</button>
+            </Link>
+            <Link to='/logout'>
+                <button className='bg-red-600 px-6 py2 rounded cursor-pointer text-white'>Logout</button>
+            </Link>
+        </div> :
+
         <div>
-            <Link to='login'>
-                <button className='text-white pr-4'>Sign In</button>
-            </Link>
-            <Link to='/signup'>
-                <button className='bg-red-600 px-6 py2 rounded cursor-pointer text-white'>Sign Up</button>
-            </Link>
-        </div>
+        <Link to='login'>
+            <button className='text-white pr-4'>Sign In</button>
+        </Link>
+        <Link to='/signup'>
+            <button className='bg-red-600 px-6 py2 rounded cursor-pointer text-white'>Sign Up</button>
+        </Link>
+    </div>}
     </div>
   )
+}
 
 export default Navbar
